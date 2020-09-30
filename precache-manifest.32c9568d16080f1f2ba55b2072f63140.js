304fd5137f99564db07030e19a5037f7e7a0f5f8
Edward <e.arekhva@gmail.com>
2020-09-30 16:38:28 +0200

    Updates

---
+++ precache-manifest.32c9568d16080f1f2ba55b2072f63140.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b8843b6174d4704b6d8f6625d83074",
+    "url": "/FilmEd/index.html"
+  },
+  {
+    "revision": "2986757b84f3f873d91c",
+    "url": "/FilmEd/static/css/main.5949a600.chunk.css"
+  },
+  {
+    "revision": "db6f684f4e9a00decba7",
+    "url": "/FilmEd/static/js/2.38c375d2.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/FilmEd/static/js/2.38c375d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2986757b84f3f873d91c",
+    "url": "/FilmEd/static/js/main.749bd3f3.chunk.js"
+  },
+  {
+    "revision": "a3a00b707560a2d57313",
+    "url": "/FilmEd/static/js/runtime-main.d3d0a3ce.js"
+  },
+  {
+    "revision": "2ac62d810ef4328203ba5adb6d71ab76",
+    "url": "/FilmEd/static/media/no-image.2ac62d81.svg"
+  },
+  {
+    "revision": "cb7e91651596604df3ffca649afb1a22",
+    "url": "/FilmEd/static/media/no-image.cb7e9165.svg"
+  },
+  {
+    "revision": "9176a06838af2c72dac0498cda8b8aa3",
+    "url": "/FilmEd/static/media/search.9176a068.svg"
+  }
+]);
